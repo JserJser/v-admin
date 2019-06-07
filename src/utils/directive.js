09588d0da882cf9ff54d896eab3848aa2d490ce5
koki <zghong1993@qq.com>
2019-06-08 01:37:25 +0800

    fix directive

--- src/utils/directive.js
+++ src/utils/directive.js
@@ -1,7 +1,8 @@
 export default Vue => {
   // btn permission control
   Vue.directive('hasPermission', {
-    bind(el) {
+    inserted(el, { value }, vnode) {
+      console.log(value, vnode.context.$store.state.roles)
       if (el.parentNode) {
         el.parentNode.removeChild(el)
       }
